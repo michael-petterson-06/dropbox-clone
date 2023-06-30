b5668f66602e3e1a0a835eafecb610d02b29b24b
Michael <michaelpetterson06.com.br>
2023-06-30 11:41:57 -0300

    Mostrar navegação no browser location

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -5,6 +5,7 @@ class DropBoxController {
             this.currentFolder = ['directory'];
             //Criando um evento para quando mudar o arquivo selecionado
             this.onselectionchange = new Event('selectionchange');
+            this.navEl = document.querySelector('#browse-location');
             this.btnSendFileEl = document.querySelector('#btn-send-file');
             this.inputFilesEl = document.querySelector('#files');
             this.snackModalEl = document.querySelector('#react-snackbar-root');
@@ -618,10 +619,65 @@ class DropBoxController {
 
             if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
         
-            // this.renderNav();
+            this.renderNav();
             this.readFiles();
     
         }
+
+        renderNav(){
+
+            let nav = document.createElement('nav');
+            let path = [];
+    
+            for (let i = 0; i < this.currentFolder.length; i++) {
+    
+                let folderName = this.currentFolder[i];
+                let span = document.createElement('span');
+    
+                path.push(folderName);
+    
+                if ((i + 1) === this.currentFolder.length) {
+    
+                    span.innerHTML = folderName;
+    
+                } else {
+    
+                    span.className = 'breadcrumb-segment__wrapper';
+                    span.innerHTML = `
+                        <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                            <a href="#" data-path=${path.join('/')} class="breadcrumb-segment">${folderName}</a>
+                        </span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                            <title>arrow-right</title>
+                            <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282"
+                                fill-rule="evenodd"></path>
+                        </svg>
+                    `;
+    
+                }
+    
+                nav.appendChild(span);
+    
+            }
+    
+            this.navEl.innerHTML = nav.innerHTML;
+    
+            // this.navEl.querySelectorAll('a').forEach(a => {
+    
+            //     a.addEventListener('click', e => {
+    
+            //         e.preventDefault();
+    
+            //         this.currentFolder = a.dataset.path.split('/');
+    
+            //         this.openFolder();
+    
+            //     });
+    
+            // });
+    
+        }
+        
 
         initEventsLi(li){
 
